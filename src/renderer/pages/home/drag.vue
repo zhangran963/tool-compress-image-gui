7dd69de4bce127a43b545544646f8620e2dfeba5
ran <zhangran963@gmail.com>
2020-04-27 09:28:03 +0800

    feat: 支持拖放压缩

--- src/renderer/pages/home/drag.vue
+++ src/renderer/pages/home/drag.vue
@@ -1,158 +1,69 @@
 <template>
 	<div class="drag-container">
-		<div class="dropzone">
-			<div id="draggable" draggable="true" ondragstart="event.dataTransfer.setData('text/plain',null)">
-				可以拖放的元素
-			</div>
-		</div>
-		<div class="dropzone main" @drop="dropHandler" @dragover="preventHandler">
-			<p>这里可接受外部拖放的文件</p>
-			<div v-if="file">
-				<p>文件名: {{ file.name }}</p>
-				<p>类型: {{ file.type }}</p>
-				<p>路径: {{ file.path }}</p>
-			</div>
-		</div>
-		<div class="dropzone" @drop="dropHandler" @dragover="preventHandler"></div>
-		<div class="dropzone" @drop="dropHandler" @dragover="preventHandler"></div>
+		<com-drag-box @change="(validFile) => {this.file = validFile}"></com-drag-box>
+
+    <div class="btn-box" v-if="file && file.isValid">
+      <button class="btn start" v-if="status === null" @click="compressHandler">开始压缩</button>
+      <button class="btn compressing" v-if="status === true">压缩中...</button>
+      <button class="btn error" v-if="status === false" @click="resetHandler">恢复</button>
+    </div>
 	</div>
 </template>
 
 <script>
+const compress = require('./node-compress/index')
+import ComDragBox from './drag-box';
+
+
 export default {
-	data() {
-		return {
-			file: null,
-		};
-	},
+	components: { ComDragBox },
+	data: () => ({
+    file: null,
+    status: null, /* 未压缩: null; 压缩中: true; 压缩出错: false */
+  }),
+  watch: {
+    file(curr){
+      console.log('* 父组件接收: ', curr)
+    }
+  },
+  methods: {
+    /* 压缩 */
+    compressHandler(){
+      this.status = true
+      compress(this.file.path).then(res => {
+        console.log('* 压缩结果', res)
 
-	watch: {
-		file: {
-			handler(curr, prev) {
-				console.log('* prevFile', prev);
-				console.log('* currFile', curr);
-			},
-		},
-	},
-	mounted() {
-		// let dragged;
-		/* 拖放内容 */
-		// document.addEventListener(
-		// 	'drag',
-		// 	function(event) {
-		// 		let { dataTransfer } = event;
-		// 		if (dataTransfer.items) {
-		// 			Array.from(dataTransfer.items).forEach((item) => {
-		// 				console.log('* item', item);
-		// 			});
-		// 		}
-		// 	},
-		// 	false
-		// );
-		// document.addEventListener(
-		// 	'dragstart',
-		// 	function(event) {
-		// 		/* 拖放内容 */
-		// 		dragged = event.target;
-		// 		event.target.style.opacity = 0.1;
-		// 	},
-		// 	false
-		// );
-		// document.addEventListener(
-		// 	'dragend',
-		// 	function(event) {
-		// 		/* 拖放内容 */
-		// 		event.target.style.opacity = '';
-		// 		let { dataTransfer } = event;
-		// 		console.log('* drag', dataTransfer.items, dataTransfer.files);
-		// 	},
-		// 	false
-		// );
-		/* 放下目标节点时触发事件 */
-		// document.addEventListener(
-		// 	'dragover',
-		// 	function(event) {
-		// 		/* 阻止默认动作 */
-		// 		event.preventDefault();
-		// 	},
-		// 	false
-		// );
-		// document.addEventListener(
-		// 	'dragenter',
-		// 	function(event) {
-		// 		/* 目标盒子, 改变背景色 */
-		// 		if (event.target.className == 'dropzone') {
-		// 			event.target.style.background = 'purple';
-		// 		}
-		// 	},
-		// 	false
-		// );
-		// document.addEventListener(
-		// 	'dragleave',
-		// 	function(event) {
-		// 		/* 目标盒子, 重置背景色 */
-		// 		if (event.target.className == 'dropzone') {
-		// 			event.target.style.background = '';
-		// 		}
-		// 	},
-		// 	false
-		// );
-		// document.addEventListener(
-		// 	'drop',
-		// 	function(event) {
-		// 		/* 阻止默认动作（如打开一些元素的链接） */
-		// 		event.preventDefault();
-		// 		/* 目标盒子, 转移 拖放的DOM */
-		// 		if (event.target.className == 'dropzone' && dragged) {
-		// 			event.target.style.background = '';
-		// 			dragged.parentNode.removeChild(dragged);
-		// 			event.target.appendChild(dragged);
-		// 		}
-		// 	},
-		// 	false
-		// );
-	},
+        /* 恢复状态 */
+        this.status = null
+      }).catch(err => {
+        console.log('* err', err)
+        this.status = false
+      })
+    },
 
-	methods: {
-		/* 外部拖放: 放下 */
-		dropHandler(event) {
-			event.preventDefault();
+    /* 重置 */
+    resetHandler(){
+      this.file = null
+      this.status = null
+    },
 
-			let { dataTransfer } = event;
-			const file = dataTransfer.files[0];
-			const { name, path, type, size } = file;
-			let res = { name, path, type, size };
-			// console.log('* drag files', res);
-			this.file = res;
-		},
-
-		/* 外部拖放: 阻止打开 */
-		preventHandler(event) {
-			/* 阻止默认动作 */
-			event.preventDefault();
-		},
-	},
+  }
 };
 </script>
 
+
 <style lang="scss" scoped>
-#draggable {
-	width: 200px;
-	height: 20px;
-	text-align: center;
-	background: white;
-}
-
-.dropzone {
-	width: 200px;
-	height: 40px;
-	background: blueviolet;
-	margin-bottom: 10px;
-	padding: 10px;
-
-	&.main {
-    width: 360px;
-		height: 100px;
-	}
+.drag-container {
+  .btn-box{
+    .btn{
+      padding: 16px 24px;
+      border: none;
+      outline: none;
+      background-color: green;
+      border-radius: 2px;
+      font-size: 20px;
+      color: white;
+    }
+  }
 }
 </style>
